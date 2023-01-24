49125b86007fae7acb76a123e5885852471b5fbf
Brian Seaver <bseaver@d155.org>
2023-01-24 14:52:35 +0000

    added a testing comment from repl.it

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Another Test Project starting on IntelliJ");
+      System.out.println("testing from repl.it")
     }
 }
